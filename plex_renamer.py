b70b378b20e97d36667b9a5c2918ef8f9ec4667e
Dylan Neff <Dyn0402@hotmail.com>
2020-07-29 22:32:55 -0700

    Added hunter x hunter to plex_renamer.py

--- plex_renamer.py
+++ plex_renamer.py
@@ -17,11 +17,12 @@ tvdb.KEYS.API_KEY = '20503FE8-5959-4849-88CC-2A0B8E9E8B2A'
 
 
 def main():
-    mandalorian()
+    hunterx()
 
 
 def naruto_box_to_season():
-    path = '/home/dylan/local_server/dyn0402/Torrents/TV Shows/Naruto Shippuden/'
+    # path = '/home/dylan/local_server/dyn0402/Torrents/TV Shows/Naruto Shippuden/'
+    path = 'C:/Users/Dylan/Torrents/TV Shows/Naruto Shippuden/'
     search = tvdb.Search()
     response = search.series('Naruto Shippuden')
     show = tvdb.Series(search.series[0]['id'])
@@ -30,26 +31,29 @@ def naruto_box_to_season():
     episodes = show.Episodes.all()
     print(episodes[300])
 
-    for box in range(31, 39):
+    for box in range(32, 34):
         box_path = path + f'Box Set {box}' + '/'
-        box_path = [x[0] for x in os.walk(box_path)][1]+'/'
+        # box_path = [x[0] for x in os.walk(box_path)][1]+'/'
+        # print(box_path)
         original_files = os.listdir(box_path)
+        # print(original_files)
         for file in original_files:
             extension = get_extension(file)
             if extension == 'txt':
                 continue
             abs_episode = get_ndig_ep(file, n=3)
-            episode = next(ep for ep in episodes if ep['absoluteNumber'] == abs_episode)
-            season = episode['airedSeason']
-            episode_name = str(episode['absoluteNumber']) + ' ' + episode['episodeName']
-            episode_name = episode_name.replace('/', 'and')
-            name = file_name_format(show.seriesName, season, episode['airedEpisodeNumber'], episode_name, extension)
-            source = box_path + file
-            dest = path + folder_name_format(season) + '/' + name
-            print(source)
-            print(dest)
-            print()
-            shutil.move(source, dest)
+            if abs_episode in range(413, 417):
+                episode = next(ep for ep in episodes if ep['absoluteNumber'] == abs_episode)
+                season = episode['airedSeason']
+                episode_name = str(episode['absoluteNumber']) + ' ' + episode['episodeName']
+                episode_name = episode_name.replace('/', 'and')
+                name = file_name_format(show.seriesName, season, episode['airedEpisodeNumber'], episode_name, extension)
+                source = box_path + file
+                dest = path + folder_name_format(season) + '/' + name
+                print(source)
+                print(dest)
+                print()
+                shutil.move(source, dest)
 
 
 def make_season_dirs(path, start, end):
@@ -58,7 +62,7 @@ def make_season_dirs(path, start, end):
 
 
 def naruto_add_ep_names():
-    path = '/home/dylan/local_server/dyn0402/Torrents/TV Shows/Naruto Shippuden/'
+    path = 'C:/Users/Dylan/Torrents/TV Shows/Naruto Shippuden/'
     search = tvdb.Search()
     response = search.series('Naruto Shippuden')
     show = tvdb.Series(search.series[0]['id'])
@@ -66,11 +70,18 @@ def naruto_add_ep_names():
     print(show.seriesName)
     episodes = show.Episodes.all()
 
-    for season_num in range(1, 18):
+    for season_num in range(17, 19):
         season_path = path + folder_name_format(season_num) + '/'
         original_files = os.listdir(season_path)
         for file in original_files:
             extension = get_extension(file)
+            # abs_episode = get_ndig_ep(file, n=3)
+            # episode = next(ep for ep in episodes if ep['absoluteNumber'] == abs_episode)
+            # season = episode['airedSeason']
+            # episode_name = str(episode['absoluteNumber']) + ' ' + episode['episodeName']
+            # episode_name = episode_name.replace('/', 'and')
+            # episode_name = episode_name.replace(':', '-')
+            # name = file_name_format(show.seriesName, season, episode['airedEpisodeNumber'], episode_name, extension)
             season, episode = get_sxxexx(file)
             episode_name = next(str(ep['absoluteNumber']) + ' ' + ep['episodeName'] for ep in episodes
                                 if ep['airedEpisodeNumber'] == episode and ep['airedSeason'] == season)
@@ -80,7 +91,8 @@ def naruto_add_ep_names():
             print(source)
             print(dest)
             print()
-            shutil.move(source, dest)
+            os.rename(source, dest)
+            # shutil.move(source, dest)
 
 
 def naruto_18():
@@ -108,7 +120,7 @@ def naruto_18():
         print(source)
         print(dest)
         print()
-        shutil.move(source, dest)
+        # shutil.move(source, dest)
 
 
 def mandalorian():
@@ -129,6 +141,35 @@ def mandalorian():
         source = path + file
         dest = path + name
         print()
+        print(source)
+        print(dest)
+        shutil.move(source, dest)
+
+
+def hunterx():
+    path = 'C:/Users/Dylan/Torrents/TV Shows/Hunter x Hunter/Series/'
+    search = tvdb.Search()
+    response = search.series('Hunter x Hunter (2011)')
+    show = tvdb.Series(search.series[0]['id'])
+    response = show.info()
+    episodes = show.Episodes.all()
+    print(episodes[0])
+    for ep in episodes:
+        print(f"{ep['airedEpisodeNumber']}  |  {ep['absoluteNumber']}")
+
+    original_files = os.listdir(path)
+    for file in original_files:
+        extension = get_extension(file)
+        season, episode_num = get_sxxexxx(file)
+        ep = next(ep for ep in episodes if ep['absoluteNumber'] == episode_num)
+        episode = ep['airedEpisodeNumber']
+        season = ep['airedSeason']
+        episode_name = check_file_name(ep['episodeName'])
+        print()
+        print(episode_name)
+        name = file_name_format(show.seriesName, season, episode, episode_name, extension)
+        source = path + file
+        dest = path + name
         print(source)
         print(dest)
         shutil.move(source, dest)
@@ -158,11 +199,29 @@ def get_sxxexx(file):
     return season, episode
 
 
+def get_sxxexxx(file):
+    name = file.split('/')[-1].lower()
+    se = re.search('s..e...', name).group(0)
+    season = int(se[1:3])
+    episode = int(se[4:7])
+
+    return season, episode
+
+
 def get_ndig_ep(file, n=3):
     name = file.split('/')[-1].lower()
     episode = int(re.search(r'(\d{%d})' % n, name).group(0))
 
     return episode
+
+
+def check_file_name(name):
+    bad_chars = ['*', '.', '"', '/', '\\', '[', ']', ':', ';', '|', ',', '?']
+    replace_char = '_'
+    for bad_char in bad_chars:
+        name = name.replace(bad_char, replace_char)
+
+    return name
 
 
 if __name__ == '__main__':
