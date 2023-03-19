3eac755157d97b20b5f91d281fc1f7ce202832bb
Dyn0402 <Dyn0402@hotmail.com>
2023-03-18 21:55:46 -0700

    Tweaks to apply_llnl.py, selenium is out

--- apply_llnl.py
+++ apply_llnl.py
@@ -8,7 +8,7 @@ Created as Misc/apply_llnl
 @author: Dylan Neff, Dylan
 """
 
-from selenium import webdriver
+# from selenium import webdriver
 
 from time import sleep
 
@@ -45,25 +45,31 @@ def pg_write_test():
 
 
 def pg_find_test():
-    # print(psz.locateOnScreen('C:/Users/Dylan/Desktop/llnl_shots/from_date.png'))
-    print(psz.locateOnScreen('C:/Users/Dylan/Desktop/llnl_shots/big_test.png'))
+    x, y = psz.locateCenterOnScreen('C:/Users/Dyn04/Desktop/llnl_apply_images/from_date.png')
+    pg.click(x=x, y=y)
+    x, y = psz.locateCenterOnScreen('C:/Users/Dyn04/Desktop/llnl_apply_images/2012.png')
+    pg.click(x=x, y=y)
+    x, y = psz.locateCenterOnScreen('C:/Users/Dyn04/Desktop/llnl_apply_images/July.png')
+    pg.click(x=x, y=y)
+    # pg.moveTo(x, y)
+    # print(psz.locateOnScreen('C:/Users/Dylan/Desktop/llnl_s/big_test.png'))
     # print(psz.locateOnScreen('C:/Users/Dylan/Desktop/llnl_shots/big_test.png', region=(280, 107, 1561, 1029),
     #       grayscale=True))
-    # pg.moveTo(x, y)
+
     print(pg.position())
 
 
-def selenium_test():
-    # Selenium got blocked after first couple tests, even when I did Captchas
-    driver_path = 'chromedriver/chromedriver_win.exe'
-    application_url = 'https://jobs.smartrecruiters.com/oneclick-ui/company/LLNL/publication/' \
-                      '2520cbac-db03-4c65-9541-a7aee2d6c26f?dcr_ci=LLNL&sid=2d92f286-613b-4daf-9dfa-6340ffbecf73'
-    driver = webdriver.Chrome(executable_path=driver_path)
-    driver.get(application_url)
-    # driver.find_element(By.XPATH, '//*[@id="first-name-input"]').send_keys('Test')
-    sleep(15)
-    driver.close()
-    driver.quit()
+# def selenium_test():
+#     # Selenium got blocked after first couple tests, even when I did Captchas
+#     driver_path = 'chromedriver/chromedriver_win.exe'
+#     application_url = 'https://jobs.smartrecruiters.com/oneclick-ui/company/LLNL/publication/' \
+#                       '2520cbac-db03-4c65-9541-a7aee2d6c26f?dcr_ci=LLNL&sid=2d92f286-613b-4daf-9dfa-6340ffbecf73'
+#     driver = webdriver.Chrome(executable_path=driver_path)
+#     driver.get(application_url)
+#     # driver.find_element(By.XPATH, '//*[@id="first-name-input"]').send_keys('Test')
+#     sleep(15)
+#     driver.close()
+#     driver.quit()
 
 
 if __name__ == '__main__':
