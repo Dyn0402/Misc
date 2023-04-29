6ec69aa98a26c4caa16c2578b220d2ff6e3c7394
Dyn0402 <40580528+Dyn0402@users.noreply.github.com>
2023-04-28 19:36:48 -0700

    multimeter_video_read.py tweaks but didn't work

--- multimeter_video_read.py
+++ multimeter_video_read.py
@@ -1,14 +1,21 @@
 import cv2
 import pytesseract
+import tensorflow as tf
+import tensorflow_hub as hub
+import numpy as np
 
 # Set up Tesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'  # Set path to Tesseract executable
 
+# Load pretrained model from TensorFlow Hub
+model_url = "https://tfhub.dev/google/imagenet/mobilenet_v2_100_224/classification/4"
+model = hub.KerasLayer(model_url, input_shape=(224, 224, 3))
+
 # Set up video capture
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('C:/Users/Dylan/Downloads/current_video.mp4')
 
 # Define ROI coordinates
-x, y, w, h = 100, 100, 200, 200
+x, y, w, h = 250, 300, 600, 450
 
 # Read first frame to get dimensions
 ret, frame = cap.read()
@@ -36,35 +43,41 @@ while (cap.isOpened()):
     # Threshold to remove noise
     _, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 
-    # Find contours
-    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    digits = pytesseract.image_to_string(thresh)
+    #Print digit string and frame timestamp
+    print(f"{digits} at {cap.get(cv2.CAP_PROP_POS_MSEC)}ms")
 
-    # Loop through contours
-    for cnt in contours:
-        # Get bounding box
-        x_cnt, y_cnt, w_cnt, h_cnt = cv2.boundingRect(cnt)
-
-        # Extract digit
-        digit = gray[y_cnt:y_cnt + h_cnt, x_cnt:x_cnt + w_cnt]
-
-        # Check if digit is large enough
-        if digit.shape[0] < 10 or digit.shape[1] < 10:
-            continue
-
-        # Resize digit to match Tesseract's input size
-        digit = cv2.resize(digit, (100, 100))
-
-        # Convert digit to string using Tesseract
-        digit_string = text_detector(digit, config=config)
-
-        # Print digit string and frame timestamp
-        print(f"{digit_string.strip()} at {cap.get(cv2.CAP_PROP_POS_MSEC)}ms")
-
-        # Draw bounding box around digit
-        cv2.rectangle(roi, (x_cnt, y_cnt), (x_cnt + w_cnt, y_cnt + h_cnt), (0, 0, 255), 2)
+    # # Find contours
+    # contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    #
+    # # Loop through contours
+    # for cnt in contours:
+    #     # Get bounding box
+    #     x_cnt, y_cnt, w_cnt, h_cnt = cv2.boundingRect(cnt)
+    #
+    #     # Extract digit
+    #     digit = gray[y_cnt:y_cnt + h_cnt, x_cnt:x_cnt + w_cnt]
+    #
+    #     # Check if digit is large enough
+    #     if digit.shape[0] < 10 or digit.shape[1] < 10:
+    #         continue
+    #
+    #     # Resize digit to match Tesseract's input size
+    #     digit = cv2.resize(digit, (100, 100))
+    #
+    #     # Convert digit to string using Tesseract
+    #     digit_string = text_detector(digit, config=config)
+    #
+    #     # Print digit string and frame timestamp
+    #     print(f"{digit_string.strip()} at {cap.get(cv2.CAP_PROP_POS_MSEC)}ms")
+    #
+    #     # Draw bounding box around digit
+    #     cv2.rectangle(roi, (x_cnt, y_cnt), (x_cnt + w_cnt, y_cnt + h_cnt), (0, 0, 255), 2)
+    #     cv2.rectangle(thresh, (x_cnt, y_cnt), (x_cnt + w_cnt, y_cnt + h_cnt), (0, 0, 255), 2)
 
     # Display frame
-    cv2.imshow('frame', roi)
+    cv2.imshow('frame', thresh)
+    # cv2.imshow('frame', roi)
 
     # Quit if 'q' key is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
