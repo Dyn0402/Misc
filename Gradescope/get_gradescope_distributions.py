167222ca5a24e15d921e52aebe8326957a3a3e7d
Dyn0402 <40580528+Dyn0402@users.noreply.github.com>
2023-05-05 20:07:15 -0700

    get_gradescope_distributions.py added an average plot

--- Gradescope/get_gradescope_distributions.py
+++ Gradescope/get_gradescope_distributions.py
@@ -28,19 +28,22 @@ from GradescopeNavigator import GradescopeDistributionGetter as GsDG
 
 def main():
     # selenium_test()
-    gsn_get_distribution()
-    # plot_lab('C:/Users/Dylan/Desktop/lab2_dists.csv')
+    # gsn_get_distribution()
+    # plot_lab('C:/Users/Dylan/Desktop/prelab3_dists.csv', prelab=True)
+    plot_lab('C:/Users/Dylan/Desktop/lab3_dists.csv', prelab=False)
     print('donzo')
 
 
 def gsn_get_distribution():
-    csv_path = 'C:/Users/Dylan/Desktop/prelab2_dists.csv'
     section_flags = ['5CL-G']
     # assignment_name = 'Week 1 Assignment & Lab Credit'
-    assignment_name = '5C Pre-Lab 2'
-    prelab = True
+    assignment_name = '5C Pre-Lab 3'
+    prelab = True if 'Pre-Lab' in assignment_name else False
     assignment_name_alt = 'lab 1'
     distribution_getter = GsDG()
+    assignment_type_name = 'prelab' if prelab else 'lab'
+    assignment_num = int(assignment_name[-1])
+    csv_path = f'C:/Users/Dylan/Desktop/{assignment_type_name}{assignment_num}_dists.csv'
     # distribution_getter = GsDG('C:/Users/Dyn04/Desktop/Creds/gradescope_creds.txt')
     df = []
     for section in distribution_getter.get_sections():
@@ -54,6 +57,7 @@ def gsn_get_distribution():
     df = pd.DataFrame(df)
     # df.to_csv('C:/Users/Dylan/Desktop/prelab2_dists.csv', index=False)
     df.to_csv(csv_path, index=False, mode='w')
+    print(csv_path)
     plot_lab(csv_path, prelab)
 
 
@@ -68,35 +72,53 @@ def plot_lab(path=None, prelab=False):
     fig_ungraded, ax_ungraded = plt.subplots(dpi=144)
     sns.countplot(x='ta', data=df[~df['graded']])
     ax_ungraded.set_title('Number Ungraded')
+    df = df[df['graded']]
 
     # df = df[df['section'] != '5CL-G30']
     fig_tas, ax_tas = plt.subplots(dpi=144)
     sns.histplot(data=df, x='score', hue='ta', multiple='stack')
     ax_tas.set_title('All Grades')
-    df = df[(df['score'] != min_grade) & (df['score'] != max_grade) & df['graded']]
+    df_scored = df[(df['score'] != min_grade) & (df['score'] != max_grade)]
     # df = df[(df['score'] != 0) & (df['score'] != 20)]
     # print(df[df['ta'] == 'Dylan'])
     fig_coures, ax_courses = plt.subplots(dpi=144)
-    sns.histplot(data=df, x='score', hue='section')
+    sns.histplot(data=df_scored, x='score', hue='section')
     ax_courses.set_title('No Min/Max Grades')
     fig_coures, ax_courses = plt.subplots(dpi=144)
-    sns.kdeplot(data=df, x='score', hue='section')
-    sns.rugplot(data=df, x='score', hue='section')
+    sns.kdeplot(data=df_scored, x='score', hue='section')
+    sns.rugplot(data=df_scored, x='score', hue='section')
     # for section in pd.unique(df['section']):
     #     df_section = df[df['section'] == section]
     #     sns.histplot(df_section['score'], label=section)
     # plt.legend()
 
     fig_tas, ax_tas = plt.subplots(dpi=144)
-    sns.histplot(data=df, x='score', hue='ta', multiple='stack')
+    sns.histplot(data=df_scored, x='score', hue='ta', multiple='stack')
     ax_tas.set_title('No Min/Max Grades')
     fig_tas, ax_tas = plt.subplots(dpi=144)
-    sns.kdeplot(data=df, x='score', hue='ta')
-    sns.rugplot(data=df, x='score', hue='ta')
+    sns.kdeplot(data=df_scored, x='score', hue='ta')
+    sns.rugplot(data=df_scored, x='score', hue='ta')
     # for ta_name in pd.unique(df['ta']):
     #     df_ta = df[df['ta'] == ta_name]
     #     sns.histplot(df_ta['score'], label=ta_name)
     # plt.legend()
+
+    fig_tas, ax_tas = plt.subplots(dpi=144)
+    ta_mean_sd_df = df.groupby('ta')['score'].agg(['mean', 'std']).reset_index()
+    ax_tas.axhline(8 if prelab else 20, color='black')
+    # ax_tas.grid()
+    ax_tas.errorbar(ta_mean_sd_df['ta'], ta_mean_sd_df['mean'], yerr=ta_mean_sd_df['std'], marker='o', ls='none')
+    ax_tas.scatter(df['ta'], df['score'], marker='_', alpha=0.2)
+    plt.xticks(rotation=45)
+    # ax_tas.set_title('No Min/Max Grades')
+    ax_tas.set_ylabel('Average Score')
+    # sns.kdeplot(data=df, x='score', hue='ta')
+    # sns.rugplot(data=df, x='score', hue='ta')
+    # for ta_name in pd.unique(df['ta']):
+    #     df_ta = df[df['ta'] == ta_name]
+    #     sns.histplot(df_ta['score'], label=ta_name)
+    # plt.legend()
+    plt.tight_layout()
 
     plt.show()
 
