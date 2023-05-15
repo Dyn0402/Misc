9e213a1076051e52eab3493f9340d7ab6c9e3781
Dylan <Dyn0402@hotmail.com>
2023-05-14 23:37:20 -0700

    exp_example_plots.py added characteristic time

--- exp_example_plots.py
+++ exp_example_plots.py
@@ -21,33 +21,33 @@ def main():
 
 
 def plot_inv_exp():
-    a, b = 3, 1
+    a, b = 3, 0.8
     ts = np.linspace(0, 5, 100)
-    plt.figure(figsize=(4, 3))
+    plt.figure(figsize=(5, 4))
     plt.axhline(0, color='black')
     plt.axvline(0, color='black')
     plt.axhline(a, color='red', ls=':')
     plt.xlabel('Time (s)')
     plt.ylabel('Voltage (V)')
     plt.plot(ts, inv_exp(ts, a, b))
+    plt.plot([0, 1 / b], [inv_exp(1 / b, a, b), inv_exp(1 / b, a, b)], color='blue', alpha=0.5)
+    plt.plot([1 / b, 1 / b], [0, inv_exp(1 / b, a, b)], color='blue', alpha=0.5)
     plt.tight_layout()
 
 
 def plot_exp_decay():
-    a, b = 3, 1
+    a, b = 3, 0.8
     ts = np.linspace(0, 5, 100)
-    plt.figure(figsize=(4, 3))
+    plt.figure(figsize=(5, 4))
     plt.axhline(0, color='black')
     plt.axvline(0, color='black')
     plt.axhline(a, color='red', ls=':')
     plt.xlabel('Time (s)')
     plt.ylabel('Voltage (V)')
     plt.plot(ts, exp_decay(ts, a, b))
+    plt.plot([0, 1 / b], [exp_decay(1 / b, a, b), exp_decay(1 / b, a, b)], color='blue', alpha=0.5)
+    plt.plot([1 / b, 1 / b], [0, exp_decay(1 / b, a, b)], color='blue', alpha=0.5)
     plt.tight_layout()
-
-
-def hotdogs(t):
-    return
 
 
 def inv_exp(t, a, b):
