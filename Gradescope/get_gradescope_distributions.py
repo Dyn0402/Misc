6f5b95fdda999d4510ed3c818e93a179772d44d1
Dyn0402 <40580528+Dyn0402@users.noreply.github.com>
2023-05-22 19:39:53 -0700

    get_gradescope_distributions.py added some features while checking distributions

--- Gradescope/get_gradescope_distributions.py
+++ Gradescope/get_gradescope_distributions.py
@@ -27,17 +27,19 @@ from GradescopeNavigator import GradescopeDistributionGetter as GsDG
 
 
 def main():
+    lab_type = 'prelab'
+    lab_num = 5
     # selenium_test()
-    # gsn_get_distribution()
-    # plot_lab('C:/Users/Dylan/Desktop/prelab3_dists.csv', prelab=True)
-    plot_lab('C:/Users/Dylan/Desktop/lab3_dists.csv', prelab=False)
+    # gsn_get_distribution(lab_type, lab_num)
+    plot_lab(f'C:/Users/Dylan/Desktop/{lab_type}{lab_num}_dists.csv', prelab=True if lab_type == 'prelab' else False)
+    # plot_lab('C:/Users/Dylan/Desktop/lab3_dists.csv', prelab=False)
     print('donzo')
 
 
-def gsn_get_distribution():
+def gsn_get_distribution(lab_type, lab_num):
     section_flags = ['5CL-G']
     # assignment_name = 'Week 1 Assignment & Lab Credit'
-    assignment_name = '5C Pre-Lab 3'
+    assignment_name = f'5C {"Pre-Lab" if lab_type == "prelab" else "Lab"} {lab_num}'
     prelab = True if 'Pre-Lab' in assignment_name else False
     assignment_name_alt = 'lab 1'
     distribution_getter = GsDG()
@@ -53,12 +55,12 @@ def gsn_get_distribution():
             if len(df_section) == 0:
                 df_section = distribution_getter.get_distribution(section, assignment_name_alt)
             df.extend(df_section)
-    distribution_getter.close()
+    # distribution_getter.close()
     df = pd.DataFrame(df)
     # df.to_csv('C:/Users/Dylan/Desktop/prelab2_dists.csv', index=False)
     df.to_csv(csv_path, index=False, mode='w')
     print(csv_path)
-    plot_lab(csv_path, prelab)
+    # plot_lab(csv_path, prelab)
 
 
 def plot_lab(path=None, prelab=False):
@@ -85,7 +87,7 @@ def plot_lab(path=None, prelab=False):
     sns.histplot(data=df_scored, x='score', hue='section')
     ax_courses.set_title('No Min/Max Grades')
     fig_coures, ax_courses = plt.subplots(dpi=144)
-    sns.kdeplot(data=df_scored, x='score', hue='section')
+    # sns.kdeplot(data=df_scored, x='score', hue='section')
     sns.rugplot(data=df_scored, x='score', hue='section')
     # for section in pd.unique(df['section']):
     #     df_section = df[df['section'] == section]
@@ -96,7 +98,7 @@ def plot_lab(path=None, prelab=False):
     sns.histplot(data=df_scored, x='score', hue='ta', multiple='stack')
     ax_tas.set_title('No Min/Max Grades')
     fig_tas, ax_tas = plt.subplots(dpi=144)
-    sns.kdeplot(data=df_scored, x='score', hue='ta')
+    # sns.kdeplot(data=df_scored, x='score', hue='ta')
     sns.rugplot(data=df_scored, x='score', hue='ta')
     # for ta_name in pd.unique(df['ta']):
     #     df_ta = df[df['ta'] == ta_name]
@@ -105,19 +107,16 @@ def plot_lab(path=None, prelab=False):
 
     fig_tas, ax_tas = plt.subplots(dpi=144)
     ta_mean_sd_df = df.groupby('ta')['score'].agg(['mean', 'std']).reset_index()
+    # ta_grouped = df.groupby('ta')
     ax_tas.axhline(8 if prelab else 20, color='black')
-    # ax_tas.grid()
+    # quartile_errorbars = [ta_mean_sd_df['mean'] - ta_grouped['score'].quantile(0.25).values,
+    #                       ta_grouped['score'].quantile(0.75).values - ta_mean_sd_df['mean']]
+    # print(ta_mean_sd_df['mean'] - ta_grouped['score'].quantile(0.25).values)
     ax_tas.errorbar(ta_mean_sd_df['ta'], ta_mean_sd_df['mean'], yerr=ta_mean_sd_df['std'], marker='o', ls='none')
+    # ax_tas.errorbar(ta_mean_sd_df['ta'], ta_mean_sd_df['mean'], yerr=quartile_errorbars, marker='o', ls='none')
     ax_tas.scatter(df['ta'], df['score'], marker='_', alpha=0.2)
     plt.xticks(rotation=45)
-    # ax_tas.set_title('No Min/Max Grades')
     ax_tas.set_ylabel('Average Score')
-    # sns.kdeplot(data=df, x='score', hue='ta')
-    # sns.rugplot(data=df, x='score', hue='ta')
-    # for ta_name in pd.unique(df['ta']):
-    #     df_ta = df[df['ta'] == ta_name]
-    #     sns.histplot(df_ta['score'], label=ta_name)
-    # plt.legend()
     plt.tight_layout()
 
     plt.show()
