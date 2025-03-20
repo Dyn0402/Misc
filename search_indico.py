1f34a29f5bc72d2b231c35e23ae0b13fcc4b0e47
Dylan <Dyn0402@hotmail.com>
2025-03-20 15:24:54 +0100

    search_indico.py added for quick poc for searching indico events

---
+++ search_indico.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on March 20 15:22 2025
+Created in PyCharm
+Created as Misc/search_indico
+
+@author: Dylan Neff, dn277127
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import requests
+
+
+def main():
+    base_url = 'https://indico.bnl.gov/event/27124/'
+    # Request page and print html
+    page = requests.get(base_url)
+    print(page)
+    # Print html content
+    # print(page.content)
+    speaker = 'Oskar Hartbrich'
+
+    # Find speaker in html
+    if speaker in page.content.decode('utf-8'):
+        print(f'{speaker} is speaking at this event.')
+    else:
+        print(f'{speaker} is not speaking at this event.')
+    print('donzo')
+
+
+if __name__ == '__main__':
+    main()
