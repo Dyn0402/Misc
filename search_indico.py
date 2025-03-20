7f2fcd29fc493e04b335740e55d7a9446334701c
Dylan <Dyn0402@hotmail.com>
2025-03-20 15:29:23 +0100

    search_indico.py put in a loop over events to see if I get banned

--- search_indico.py
+++ search_indico.py
@@ -15,19 +15,20 @@ import requests
 
 
 def main():
-    base_url = 'https://indico.bnl.gov/event/27124/'
-    # Request page and print html
-    page = requests.get(base_url)
-    print(page)
-    # Print html content
-    # print(page.content)
-    speaker = 'Oskar Hartbrich'
+    for i in range(20000, 27124):
+        base_url = f'https://indico.bnl.gov/event/{i}/'
+        # Request page and print html
+        page = requests.get(base_url)
+        # print(page)
+        # Print html content
+        # print(page.content)
+        speaker = 'Oskar Hartbrich'
 
-    # Find speaker in html
-    if speaker in page.content.decode('utf-8'):
-        print(f'{speaker} is speaking at this event.')
-    else:
-        print(f'{speaker} is not speaking at this event.')
+        # Find speaker in html
+        if speaker in page.content.decode('utf-8'):
+            print(f'{speaker} is speaking at this event {i}.')
+        else:
+            print(f'{speaker} is not speaking at this event {i}.')
     print('donzo')
 
 
